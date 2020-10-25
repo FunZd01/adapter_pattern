b44bfd16a07244f972a3c17ada92edc3e3fe940b
funz01 <domingofunez99@gmail.com>
2020-10-25 17:27:54 -0600

    structure project

---
+++ src/main/java/DocumentoPdf.java
@@ -0,0 +1,17 @@
+public class DocumentoPdf {
+    protected ComponentePdf herramientaPdf = new ComponentePdf();
+    public void setContenido(String contenido)
+    {
+        herramientaPdf.pdfFijaContenido(contenido);
+    }
+    public void dibuja()
+    {
+        herramientaPdf.pdfPreparaVisualizacion();
+        herramientaPdf.pdfRefresca();
+        herramientaPdf.pdfFinalizaVisualizacion();
+    }
+    public void imprime()
+    {
+        herramientaPdf.pdfEnviaImpresora();
+    }
+}
